4a6c417f1c67a54f767cc055e6568b2a11281de9
Camille Kander <camille@kander.com>
2016-02-12 12:09:22 -0800

    Create project

---
+++ Busbud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Busbud
+//
+//  Created by Camille Kander on 2/12/16.
+//  Copyright © 2016 xzoky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
