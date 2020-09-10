630399ef019abb8046bed0841dc1880aab9407b5
afonsomarquez <ext-l.lecona@rappi.com>
2020-09-10 09:34:34 -0400

    Create Base SwiftUI Project

---
+++ BBChallenge/BBChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BBChallenge
+//
+//  Created by Luis Alfonso Marquez Lecona on 2020-09-10.
+//  Copyright © 2020 Busbud. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
