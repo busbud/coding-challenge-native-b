87e584b00e432d7d455150d47390df936c7c8b84
sajjadsarkoobi <sajjadsarkoobi@gmail.com>
2020-09-16 15:27:25 +0300

    Initial Commit

---
+++ BusBud/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BusBud
+//
+//  Created by Sajjad Sarkoobi on 9/16/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
