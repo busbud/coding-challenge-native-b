bdc8642473434801b2c0894a70405a99c8299159
Nitinkachhadiya <nitin@visibly.io>
2020-09-26 11:52:24 +0530

    Add initial iOS projects and gitignore file

---
+++ BBServices/BBServices/BBServices.h
@@ -0,0 +1,19 @@
+//
+//  BBServices.h
+//  BBServices
+//
+//  Created by Nitin kachhadiya on 25/09/20.
+//  Copyright © 2020 Nitin kachhadiya. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BBServices.
+FOUNDATION_EXPORT double BBServicesVersionNumber;
+
+//! Project version string for BBServices.
+FOUNDATION_EXPORT const unsigned char BBServicesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BBServices/PublicHeader.h>
+
+
